7a274338158d0af996321929ad910ed3863e6000
Yue Gao <hologerry@gmail.com>
2023-06-12 20:35:22 +0800

    add scallop

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117
 pip install -r requirements.txt
 pip install kaolin==0.13.0 -f https://nvidia-kaolin.s3.us-east-2.amazonaws.com/torch-1.13.1_cu117.html
+pip install imageio-ffmpeg
